034c064dda1724ff59bca09e22e49fa85369003f
praveen6228 <104729165+praveen6228@users.noreply.github.com>
2023-01-24 13:23:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+
+  function updateWordCount() {
+    var text = document.getElementById("evaluatedText").value;
+    var wordCount = text.split(" ").length;
+    document.getElementById("wordCount").innerHTML = wordCount;
+  }
+  document.getElementById("evaluatedText").addEventListener("input", updateWordCount);
+
+
+  function updateWordCount() {
+    var text = document.getElementById("evaluatedText").value;
+    var wordCount = text.trim() == "" ? 0 : text.split(" ").length;
+    document.getElementById("wordCount").innerHTML = wordCount;
+  }
+  document.getElementById("evaluatedText").addEventListener("input", updateWordCount);
+
